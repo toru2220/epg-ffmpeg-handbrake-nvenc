03f66414e201591866a192a2a8026c2491da7972
toru2220 <toru2220@gmail.com>
2023-03-01 11:37:14 +0900

    ffff

--- handbrake.sh
+++ handbrake.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#if [ $# != 4 ]; then
-#    echo "Usage: encode.sh keepFile targetDir targetExt outputDir encodeOpt encodedLogFile"
-#    exit 0
-#fi
-
 file=${1:-1}
 ENCODE_OPT=${ENCODE_OPT_DEFAULT:-"-e nvenc_h264 -r 29.97 --pfr -E faac -B 160 -6 dpl2 -R Auto -D 0.0 -f mp4 --crop 0:0:0:0 --loose-anamorphic -m --decomb -x cabac=0:ref=2:me=umh:bframes=0:weightp=0:subme=6:8x8dct=0:trellis=0 -O --all-audio --all-subtitles"}
 KEEP_FILE=${KEEP_FILE_DEFAULT:-"1"}
@@ -43,16 +38,15 @@ OUTPUT_DIR=${OUTPUT_DIR_DEFAULT:-"/data/dlna"}
 
   title_count=$((title_count+1))
 
+  echo "encode result"
+  echo `ls -lh "${file}"`
+  echo `ls -lh "${destfile}"`
  done
 
  if [ ${max_titles} != ${title_count} ]; then
   echo "encode uncompleted. please delete fragment file manually."
   exit 0
  fi
- 
- echo "encode result"
- echo `ls -l "${file}"`
- echo `ls -l "${destfile}"`
 
  if [ 0 = ${KEEP_FILE} ]; then
   echo "delete original file:${file}"
